16a12526113a4e106354f0c2cd211b9415855ae2
kbjude <kbjude@gmail.com>
2020-06-03 01:47:26 +0300

    user signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(user_params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :user, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show]
+  root 'users#new'
 end
