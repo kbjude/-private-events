fa901ac2c4c79fc2d0975c447ed5d32a36b2d0e3
kbjude <kbjude@gmail.com>
2020-06-05 00:03:06 +0300

    even controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_214720) do
+ActiveRecord::Schema.define(version: 2020_06_04_202217) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
