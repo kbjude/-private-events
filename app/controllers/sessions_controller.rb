46f8e22fed9c5b9fa385c17934bfbcf9436b4f9d
kbjude <kbjude@gmail.com>
2020-06-04 20:34:12 +0300

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,10 +15,10 @@ class SessionsController < ApplicationController
         end
     end
 
-    # def destroy
-    #     session[:user_id] = nil
-    #     redirect_to root_path 
-    # end
+     def destroy
+         session[:user_id] = nil
+         redirect_to root_path 
+     end
 
     def show;end
 
