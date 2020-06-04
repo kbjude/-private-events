78929c19313a692b3e1486dd7c2f1b104e1700e2
kbjude <kbjude@gmail.com>
2020-06-05 00:09:13 +0300

    params values

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,18 @@
 class EventsController < ApplicationController
     def index
+        @events = Event.all
     end
 
     def new
+        @event = Event.new
     end
 
     def create
+        
     end
 
     def show
+        @event = Event.find(params[:id])
     end
     params.require(:event).permit(:name, :description, :date, :user_id)
 end
