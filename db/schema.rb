a1ac54388e158f8e74da2281853b31bc0814cf76
kbjude <kbjude@gmail.com>
2020-06-06 15:18:28 +0300

    many to many table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_004521) do
+ActiveRecord::Schema.define(version: 2020_06_05_150743) do
 
   create_table "attended_events", force: :cascade do |t|
     t.integer "user_id"
