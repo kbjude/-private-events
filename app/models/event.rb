148ea7d63268e22933935ab6850a8c0b8a575a9c
kbjude <kbjude@gmail.com>
2020-06-08 21:09:06 +0300

    event owner

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,8 @@ class Event < ApplicationRecord
 
     scope :upcoming_date, -> { select { |event| event.date >= Time.zone.now } }
     scope :past_date, -> { select { |event| event.date < Time.zone.now } }
+
+    def self.creator(event)
+        User.where(id: event.user_id)
+    end
 end
