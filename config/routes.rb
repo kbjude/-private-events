4b51c63f3679754295ec5a43332accc3dcbc33f8
kbjude <kbjude@gmail.com>
2020-06-11 21:13:17 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create show]
-  resources :events
-  resources :sessions, only: %i[create new destroy show]
+  resources :events, only: %i[new create show index]
+  resources :sessions, only: %i[create new destroy ]
   get '/signup', to: 'users#new'
   get '/logout', to: 'sessions#destroy'
   get '/signin', to: 'sessions#new'
