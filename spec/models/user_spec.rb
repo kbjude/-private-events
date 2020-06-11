0accae23399fa61414a43ca734419cf7b6208693
kbjude <kbjude@gmail.com>
2020-06-11 19:58:12 +0300

    integration complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe User, type: :feature do
       visit new_user_path
       expect(page).to have_content('Signin')
     end
+
+    it 'User us redirected to the index page' do
+      visit events_path
+      expect(page).to have_content('Events Happening')
+    end
   end
 end
